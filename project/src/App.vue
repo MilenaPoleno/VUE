8190b661c2b991a983bbf7950cb9d6180e35c368
Milena <emilieguryeva@gmail.com>
2023-10-29 20:38:54 +0300

    404

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <HeaderComponent/>
-  <IndexComponent/>
-  <FooterComponent/>
-  <HeaderComponent/>
-  <ArticleComponent/>
-  <FooterComponent/>
-  <HeaderComponent/>
-  <BlogDetailsComponent/>
-  <FooterComponent/>
+  <router-view/>
 </template>
 
 <script>
-import IndexComponent from './components/IndexComponent.vue'
-import ArticleComponent from './components/ArticleComponent.vue';
-import BlogDetailsComponent from './components/BlogDetailsComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    IndexComponent, ArticleComponent, BlogDetailsComponent,
     HeaderComponent,
-    FooterComponent
 }
 }
 </script>
